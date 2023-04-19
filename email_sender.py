a2a9021930aede15e304b016d4faf66044099fe0
nstonic <nstonic@gmail.com>
2023-04-19 09:58:50 +0400

    Change environs

--- email_sender.py
+++ email_sender.py
@@ -10,22 +10,14 @@ from dotenv import load_dotenv
 def send_email(msg_body: str, subject: str, receiver: str):
     load_dotenv()
     smtpobj = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    smtpobj.login(os.environ['LOGIN'], os.environ['PASSWORD'])
+    smtpobj.login(os.environ['EMAIL_LOGIN'], os.environ['EMAIL_PASSWORD'])
     smtpobj.set_debuglevel(True)
 
     msg = MIMEMultipart()
-    msg['From'] = os.environ['LOGIN']
+    msg['From'] = os.environ['EMAIL_LOGIN']
     msg['To'] = receiver
     msg['Subject'] = subject
     msg.attach(MIMEText(msg_body, 'plain'))
 
     smtpobj.send_message(msg)
     smtpobj.quit()
-
-
-if __name__ == '__main__':
-    send_email(
-        msg_body='Привет',
-        subject='Привет',
-        receiver='ns-tonic@yandex.ru'
-    )
