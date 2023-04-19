28342ab28155bf2113815e181bd25da81497c6ba
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-19 22:04:09 +0500

    register Storage and StorageImage models

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Storage, StorageImage
+
+
+admin.site.register(StorageImage)
+
+
+class StorageImageInline(admin.TabularInline):
+    model = StorageImage
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return format_html(
+            '<img src="{url}" width="200" />'.format(
+                url=obj.image.url
+            )
+        )
+    fields = ('image', 'preview_image')
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['city', 'street']}
+    inlines = [StorageImageInline]
+    fields = (
+        'city',
+        'street',
+        'building',
+        'specificity',
+        'description',
+        'longitude',
+        'latitude',
+        'slug'
+    )
