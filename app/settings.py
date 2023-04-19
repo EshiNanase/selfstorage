ef69f214f1eb1065b2e4835e28f2a8314d876bf8
nstonic <nstonic@gmail.com>
2023-04-19 09:53:56 +0400

    Add defualt value to DEBUG

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', False)
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
