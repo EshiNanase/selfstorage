88ab477fc75d0f20f698008efce1eb998cc772cb
nstonic <118763806+nstonic@users.noreply.github.com>
2023-04-19 21:29:23 +0400

    Add email notifier command

--- email_sender.py
+++ email_sender.py
@@ -1,23 +1,43 @@
-import os
 import smtplib
+import sys
+from datetime import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 # from email.mime.image import MIMEImage  # Изображения
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from django.db.models import QuerySet
 
-from dotenv import load_dotenv
+from personal_account.models import Client
 
 
-def send_email(msg_body: str, subject: str, receiver: str):
-    load_dotenv()
-    smtpobj = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    smtpobj.login(os.environ['EMAIL_LOGIN'], os.environ['EMAIL_PASSWORD'])
-    smtpobj.set_debuglevel(True)
+def send_email(msg_body: str, subject: str, receivers: QuerySet[Client]):
+    email_validator = EmailValidator()
+
+    sender_email = settings['EMAIL_LOGIN']
+    sender_password = settings['EMAIL_PASSWORD']
+    smtp_server = settings['SMTP_SERVER']
+    smtp_server_port = settings['SMTP_PORT', 465]
+
+    smtpobj = smtplib.SMTP_SSL(smtp_server, smtp_server_port)
+    smtpobj.login(sender_email, sender_password)
 
     msg = MIMEMultipart()
-    msg['From'] = os.environ['EMAIL_LOGIN']
-    msg['To'] = receiver
+    msg['From'] = sender_email
     msg['Subject'] = subject
-    msg.attach(MIMEText(msg_body, 'plain'))
 
-    smtpobj.send_message(msg)
+    for receiver in receivers:
+        receiver_email = receiver.email
+        try:
+            email_validator(receiver_email)
+        except ValidationError:
+            continue
+
+        msg['To'] = receiver_email
+        msg.attach(MIMEText(msg_body, 'plain'))
+        smtpobj.send_message(msg)
+        time.sleep=5
+
     smtpobj.quit()
---
+++ rents/management/commands/email_notifier.py
@@ -0,0 +1,43 @@
+from datetime import timedelta
+
+from django.core.management import BaseCommand
+from django.utils.timezone import now
+
+from personal_account.models import Client
+from email_sender import send_email
+
+EXPIRED_NOTIFICATION = {
+    'subject': 'Аренда закончилась',
+    'text': 'Срок вашей аренды истек. Оплатите продление аренды, либо заберите вещи.'
+}
+
+SOON_EXPIRING_NOTIFICATION = {
+    'subject': 'Аренда скоро заканчивается',
+    'text': 'Срок вашей аренды истекает завтра. Не забудьте забрать вещи, либо оплатите продление аренды'
+}
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        right_now = now()
+        tomorrow = right_now + timedelta(days=1)
+
+        clients_with_expired_rent = Client.objects. \
+            filter(rents__expired_at__gte=right_now)
+
+        clients_with_soon_expiring_rent = Client. \
+            objects.filter(rents__expired_lte=tomorrow). \
+            exclude(rents__expired_at__gte=right_now)
+
+        send_email(
+            receivers=clients_with_expired_rent,
+            msg_body=EXPIRED_NOTIFICATION['subject'],
+            subject=EXPIRED_NOTIFICATION['text']
+        )
+
+        send_email(
+            receivers=clients_with_soon_expiring_rent,
+            msg_body=SOON_EXPIRING_NOTIFICATION['subject'],
+            subject=SOON_EXPIRING_NOTIFICATION['text']
+        )
