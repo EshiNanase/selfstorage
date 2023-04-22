33da919ea509aa54b6d7f686c5587cc382e46d19
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-22 14:36:32 +0500

    new category added in context

--- storage/views.py
+++ storage/views.py
@@ -82,23 +82,35 @@ def boxes_on_storage(request, slug):
             'building': selected_storage.building,
             'slug': selected_storage.slug,
             'images': [item.image.url for item in selected_storage.images.all()],
-            'avaliable_boxes': [],
+            'avaliable_boxes': {
+                'all_boxes': [],
+                'to3_area_boxes': [],
+                'from3_to10_area_boxes': [],
+                'from10_area_boxes': []
+            },
             'celsius_temperature': selected_storage.celsius_temperature,
             'boxes_amount': selected_storage.boxes.count,
             'box_min_price': selected_storage.boxes.aggregate(Min('price'))['price__min'],
             'box_max_height': round(selected_storage.boxes.aggregate(Max('height'))['height__max'] or 0 / 100, 1)
     }
     avaliable_boxes = selected_storage.get_free_boxes()
-    for box in avaliable_boxes:
-        box_desc = {
-            'number': box.number,
-            'price': box.price,
-            'width': round(box.width / 100, 1),
-            'height': round(box.height / 100, 1),
-            'depth': round(box.depth / 100, 1),
-            'sq_m_area': round(box.width/100 * box.depth/100, 1)
-        }
-        context['selected_storage']['avaliable_boxes'].append(box_desc)
+    if avaliable_boxes:
+        for box in avaliable_boxes:
+            box_desc = {
+                'number': box.number,
+                'price': box.price,
+                'width': round(box.width / 100, 1),
+                'height': round(box.height / 100, 1),
+                'depth': round(box.depth / 100, 1),
+                'sq_m_area': round(box.width/100 * box.depth/100, 1)
+            }
+            context['selected_storage']['avaliable_boxes']['all_boxes'].append(box_desc)
+            if box_desc['sq_m_area'] < 3:
+                context['selected_storage']['avaliable_boxes']['to3_area_boxes'].append(box_desc)
+            elif 3 <= box_desc['sq_m_area'] < 10:
+                context['selected_storage']['avaliable_boxes']['from3_to10_area_boxes'].append(box_desc)
+            else:
+                context['selected_storage']['avaliable_boxes']['from10_area_boxes'].append(box_desc)
 
     for storage in storages:
         storage_desc = {
@@ -108,7 +120,6 @@ def boxes_on_storage(request, slug):
             'building': storage.building,
             'thumbnail_image': storage.thumbnail_image.url if storage.thumbnail_image else '/static/img/storage_preview.png',
             'slug': storage.slug,
-            'images': [item.image.url for item in storage.images.all()],
             'avaliable_boxes': storage.get_free_boxes(),
             'boxes_amount': storage.boxes.count,
             'box_min_price': storage.boxes.aggregate(Min('price'))['price__min'],
