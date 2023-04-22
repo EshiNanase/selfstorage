d266aef6ea451e36db470072f9f8207e7ff3c10b
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-22 14:12:13 +0500

    storage filtering added

--- storage/urls.py
+++ storage/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from storage.views import faq, index, boxes
+from storage.views import faq, index, boxes_on_storage, storages
 
 urlpatterns = [
     path('faq/', faq, name='faq'),
     path('', index, name='index'),
-    path('boxes/', boxes, name='boxes'),
+    path('storages/', storages, name='storages'),
+    path('boxes/<slug>', boxes_on_storage, name='boxes_on_storage'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storage/views.py
+++ storage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Min, Max
 from services.geocoder import find_closest_storage
 import requests
@@ -33,16 +33,64 @@ def index(request):
     return render(request, 'index.html', {'storage': closest_storage, 'free_boxes': len(free_boxes), 'total_boxes': len(total_boxes), 'lowest_price': lowest_price})
 
 
-def boxes(request):
+def storages(request):
     storages = Storage.objects.all()
     context = {
         'storages': []
     }
 
     for storage in storages:
-
         storage_desc = {
-            'description': storage.description,
+            'specificity': storage.specificity,
+            'city': storage.city,
+            'street': storage.street,
+            'building': storage.building,
+            'thumbnail_image': storage.thumbnail_image.url if storage.thumbnail_image else '/static/img/storage_preview.png',
+            'slug': storage.slug,
+            'avaliable_boxes': storage.get_free_boxes(),
+            'boxes_amount': storage.boxes.count,
+            'box_min_price': storage.boxes.aggregate(Min('price'))['price__min'],
+        }
+        context['storages'].append(storage_desc)
+
+    return render(request, 'storages.html', context=context)
+
+
+def boxes_on_storage(request, slug):
+    storages = Storage.objects.all()
+    selected_storage = get_object_or_404(Storage, slug=slug)
+
+    context = {
+        'storages': []
+    }
+    context['selected_storage'] = {
+            'description': selected_storage.description,
+            'specificity': selected_storage.specificity,
+            'city': selected_storage.city,
+            'street': selected_storage.street,
+            'building': selected_storage.building,
+            'slug': selected_storage.slug,
+            'images': [item.image.url for item in selected_storage.images.all()],
+            'avaliable_boxes': [],
+            'celsius_temperature': selected_storage.celsius_temperature,
+            'boxes_amount': selected_storage.boxes.count,
+            'box_min_price': selected_storage.boxes.aggregate(Min('price'))['price__min'],
+            'box_max_height': round(selected_storage.boxes.aggregate(Max('height'))['height__max'] or 0 / 100, 1)
+    }
+    avaliable_boxes = selected_storage.get_free_boxes()
+    for box in avaliable_boxes:
+        box_desc = {
+            'number': box.number,
+            'price': box.price,
+            'width': round(box.width / 100, 1),
+            'height': round(box.height / 100, 1),
+            'depth': round(box.depth / 100, 1),
+            'sq_m_area': round(box.width/100 * box.depth/100, 1)
+        }
+        context['selected_storage']['avaliable_boxes'].append(box_desc)
+
+    for storage in storages:
+        storage_desc = {
             'specificity': storage.specificity,
             'city': storage.city,
             'street': storage.street,
@@ -50,24 +98,10 @@ def boxes(request):
             'thumbnail_image': storage.thumbnail_image.url if storage.thumbnail_image else '/static/img/storage_preview.png',
             'slug': storage.slug,
             'images': [item.image.url for item in storage.images.all()],
-            'avaliable_boxes': [],
-            'celsius_temperature': storage.celsius_temperature,
+            'avaliable_boxes': storage.get_free_boxes(),
             'boxes_amount': storage.boxes.count,
             'box_min_price': storage.boxes.aggregate(Min('price'))['price__min'],
-            'box_max_height': round(storage.boxes.aggregate(Max('height'))['height__max'] or 0 / 100, 1)
         }
-        avaliable_boxes = storage.get_free_boxes()
-        for box in avaliable_boxes:
-            box_desc = {
-                'number': box.number,
-                'price': box.price,
-                'width': box.width,
-                'height': box.height,
-                'depth': box.depth,
-                'sq_m_area': round(box.width*box.depth / 100, 1)
-            }
-            storage_desc['avaliable_boxes'].append(box_desc)
-
         context['storages'].append(storage_desc)
 
     return render(request, 'boxes.html', context=context)
